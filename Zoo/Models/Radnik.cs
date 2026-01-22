1cfd9612f6323831b9952f6f10cc04af70cc8598
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-01-22 15:58:25 +0100

    dodan kljuc i using

--- Zoo/Models/Radnik.cs
+++ Zoo/Models/Radnik.cs
@@ -1,11 +1,16 @@
-﻿namespace Zoo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo.Models;
 
 public class Radnik
 {
+    [Key]
     public int ID_radnika { get; set; }
+
     public string ime { get; set; } = null!;
     public string prezime { get; set; } = null!;
     public string kontakt_broj { get; set; } = null!;
+
     public int? ID_obrazovanja { get; set; }
     public string? tip_radnika { get; set; }
     public int ID_zoo { get; set; }
@@ -13,8 +18,7 @@ public class Radnik
     public Obrazovanje? Obrazovanje { get; set; }
     public Zooloski Zoo { get; set; } = null!;
 
-    public ICollection<Raspored> Rasporedi { get; set; } = new List<Raspored>();
     public ICollection<Obaveza> Obaveze { get; set; } = new List<Obaveza>();
+    public ICollection<Raspored> Rasporedi { get; set; } = new List<Raspored>();
     public ICollection<Tura> Ture { get; set; } = new List<Tura>();
 }
-
