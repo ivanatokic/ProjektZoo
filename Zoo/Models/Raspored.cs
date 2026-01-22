a00d4f49d538a9207606cbc19ab91051a8d77673
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-01-22 15:57:17 +0100

    dodann kljuc i using na vrhu

--- Zoo/Models/Raspored.cs
+++ Zoo/Models/Raspored.cs
@@ -1,13 +1,16 @@
-﻿namespace Zoo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo.Models;
 
 public class Raspored
 {
+    [Key]
     public int ID_rasporeda { get; set; }
+
     public int ID_radnika { get; set; }
     public DateTime datum { get; set; }
-    public string? smjena { get; set; }   // char(2)
+    public string? smjena { get; set; }
     public string? status { get; set; }
 
     public Radnik Radnik { get; set; } = null!;
 }
-
