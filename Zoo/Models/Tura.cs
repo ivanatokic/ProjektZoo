a589da0de3092ea0612906cb0324d531c66ce23e
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-01-22 16:00:08 +0100

    dodan kljuc i using

--- Zoo/Models/Tura.cs
+++ Zoo/Models/Tura.cs
@@ -1,8 +1,12 @@
-﻿namespace Zoo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo.Models;
 
 public class Tura
 {
+    [Key]
     public int ID_ture { get; set; }
+
     public DateTime datum { get; set; }
     public int? broj_posjetitelja { get; set; }
     public int? ID_vodica { get; set; }
