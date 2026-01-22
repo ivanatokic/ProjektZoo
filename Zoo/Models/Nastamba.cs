19fe08cb5b01f62a27b6919589e59f0cc32d3eb3
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-01-22 15:55:02 +0100

    dodan kljuc i popravljeni atributi iz baze(goemtija->ID_oblika)

--- Zoo/Models/Nastamba.cs
+++ Zoo/Models/Nastamba.cs
@@ -1,15 +1,20 @@
-﻿namespace Zoo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo.Models;
 
 public class Nastamba
 {
+    [Key]
     public int ID_nastambe { get; set; }
+
     public string oznaka { get; set; } = null!;
     public string? opis { get; set; }
     public string? sjenčenje { get; set; }
-    public string? geometrija { get; set; }
+
+    public int? ID_oblika { get; set; }
+    public Oblik? Oblik { get; set; }
 
     public ICollection<Jedinka> Jedinke { get; set; } = new List<Jedinka>();
     public ICollection<Skupina> Skupine { get; set; } = new List<Skupina>();
     public ICollection<IncidentNastamba> IncidentNastambe { get; set; } = new List<IncidentNastamba>();
 }
-
