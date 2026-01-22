076a77f5201ab9b063db5f45cde7be8157bbadbf
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-01-22 15:56:25 +0100

    dodan kljuc i using na vrhu

--- Zoo/Models/Obrazovanje.cs
+++ Zoo/Models/Obrazovanje.cs
@@ -1,11 +1,14 @@
-﻿namespace Zoo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo.Models;
 
 public class Obrazovanje
 {
+    [Key]
     public int ID_obrazovanja { get; set; }
+
     public string naziv { get; set; } = null!;
     public bool? trajno { get; set; }
 
     public ICollection<Radnik> Radnici { get; set; } = new List<Radnik>();
 }
-
