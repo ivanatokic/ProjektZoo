5e4d1ac7b0ff3557b8fc157651bb3f25b767e234
Gabrijela Grbešić <gabrijelagrbesic5@gmail.com>
2026-01-22 16:00:26 +0100

    dodan kljuc i using

--- Zoo/Models/Zooloski.cs
+++ Zoo/Models/Zooloski.cs
@@ -1,12 +1,15 @@
-﻿namespace Zoo.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Zoo.Models;
 
 public class Zooloski
 {
+    [Key]
     public int ID_zoo { get; set; }
+
     public string naziv { get; set; } = null!;
     public string adresa { get; set; } = null!;
     public int? radno_vrijeme { get; set; }
 
     public ICollection<Radnik> Radnici { get; set; } = new List<Radnik>();
 }
-
